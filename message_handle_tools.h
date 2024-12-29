65e2a451854e0bf1b14b0b39b10480dd30ae70d6
Flori Kusari <149197022+goldpirat@users.noreply.github.com>
2024-12-29 18:40:38 +0100

    Final push

--- message_handle_tools.h
+++ message_handle_tools.h
@@ -1,34 +1,28 @@
-#ifndef MESSAGE_HANDLE_TOOLS
-#define MESSAGE_HANDLE_TOOLS
+#ifndef MESSAGE_HANDLE_TOOLS_H 
+#define MESSAGE_HANDLE_TOOLS_H
 
-// some shared libraries from server.c and the message_handle_tools.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
 
-typedef enum {
-    GET, 
-    POST
-} request_types;
+typedef enum { GET, POST, UNSUPPORTED } request_types; 
 
 struct message {
     char *line;
     char *headers;
     char *body;
-    request_types request_type;
+    request_types type;  // Renamed request_type to type
 };
 
-// functions shared betwen 'server.c' and 'message_handle_tools.c'
-int parse_request(struct message *req, char *req_buff, int req_size);
+// Function prototypes with const correctness
+int parse_request(struct message *req, const char *req_buff, int req_size);
 void message_cleanup(struct message *msg);
 int create_response(struct message *req, struct message *resp, pthread_mutex_t *POST_lock);
 
-// functions shared betwen 'api_example.c' and 'message_handle_tools.c'
-int parse_request(struct message *req, char *req_buff, int req_size);
-char *parse_key_value(char *pairs, char *target, char *pair_seperator, char key_value_seperator);
+char *parse_key_value(const char *pairs, const char *target, const char *pair_seperator, char key_value_seperator);
 
-// add api endpoint function prototypes here
-char *create_user(char *data);
+// API endpoint function prototypes
+char *create_user(const char *data);  // Added const
 
 #endif
